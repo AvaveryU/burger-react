713aa985c98e6d84c1bb76d92863e417a677bffa
Avavery <juliya.belka.19@yandex.ru>
2022-04-26 23:25:23 +0300

    начальное состояние проекта

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Button} from '@ya.praktikum/react-developer-burger-ui-components';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+    <Button type="primary" size="medium">
+      Я кнопка из UI системы Yandex
+    </Button>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
