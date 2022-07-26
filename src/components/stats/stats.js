2d0827fef78e3c3dd1773990d1a0017d210f0e67
AvaveryU <juliya.belka.19@yandex.ru>
2022-07-26 15:50:34 +0300

    feed-card

--- src/components/stats/stats.js
+++ src/components/stats/stats.js
@@ -1,6 +1,33 @@
 //на странице /feed статистика заказов
 import styles from "./stats.module.css";
 const Stats = () => {
-  return <div className={`${styles.stats_box} `}></div>;
+  return (
+    <div className={`${styles.stats_box} ml-15 mt-25`}>
+      <div className={`${styles.stats_boards}`}>
+        <span className={`text text_type_main-medium`}>Готовы:</span>
+        <span className={`text text_type_main-medium`}>В работе:</span>
+        <ul className={`${styles.stats_done}`}>
+          <li className={`${styles.stats_item} text text_type_digits-default mb-2`}>034533</li>
+          <li className={`${styles.stats_item} text text_type_digits-default mb-2`}>034533</li>
+          <li className={`${styles.stats_item} text text_type_digits-default mb-2`}>034533</li>
+          <li className={`${styles.stats_item} text text_type_digits-default mb-2`}>034533</li>
+          <li className={`${styles.stats_item} text text_type_digits-default mb-2`}>034533</li>
+        </ul>
+        <ul className={`${styles.stats_inwork}`}>
+          <li className={`text text_type_digits-default mb-2`}>034538</li>
+          <li className={`text text_type_digits-default mb-2`}>034538</li>
+          <li className={`text text_type_digits-default mb-2`}>034538</li>
+        </ul>
+      </div>
+      <div className={`${styles.stats_completed}`}>
+        <span className={`text text_type_main-medium`}>Выполнено за все время:</span>
+        <span className={`${styles.stats_digits} text text_type_digits-large`}>28 752</span>
+      </div>
+      <div className={`${styles.stats_completed}`}>
+        <span className={`text text_type_main-medium`}>Выполнено за сегодня:</span>
+        <span className={`${styles.stats_digits} text text_type_digits-large`}>138</span>
+      </div>
+    </div>
+  );
 };
 export default Stats;
